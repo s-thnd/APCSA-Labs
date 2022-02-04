efbaedc039262372dfca49d70659f80739192fb2
s-thnd <98992429+s-thnd@users.noreply.github.com>
2022-02-03 19:05:42 -0600

    FilesLab.java
    I was feeling lazy + I had enough bonus points to slack off on this lab. I might at some point go back and complete the full lab and update it here.

---
+++ Text Files Lab (85 POINTS ONLY)/FilesLab.java
@@ -0,0 +1,111 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FilesLab 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			// run 70-point version
+			processIntegers("test1.txt", "output1.txt");
+			System.out.println();
+			
+			// run 85-point version
+			testLines("test2a.txt", "output2a.txt");
+			
+			// run 85-point version
+			testLines("test2b.txt", "output2b.txt");
+			System.out.println();
+			
+			// run 100-point version - good data
+			extractSubstrings("test3a.txt", "output3a.txt");		
+			
+			// run 100-point version - bad data
+			extractSubstrings("test3b.txt", "output3b.txt");		
+			System.out.println();
+		}
+		catch (IOException exc)
+		{
+			System.out.println("Exception occurred: " + exc.getMessage());
+			exc.printStackTrace();			
+		}
+	}
+	
+	public static void processIntegers(String inFile, String outFile) throws FileNotFoundException
+	{
+		
+		PrintWriter myFile = new PrintWriter(outFile);
+		Scanner scan = new Scanner(new File(inFile));
+		int numLines = scan.nextInt();
+		
+		int biggest = 0;
+		int smallest = 0;
+		
+		for(int i = 0; i < numLines; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			int c = scan.nextInt();
+			
+			if(a > b && a > c) {
+				biggest = a;
+			}
+			else if(b > c) {
+				biggest = b;
+			}
+			else {
+				biggest = c;
+			}
+			
+			if(a < b && a < c) {
+				smallest = a;
+			}
+			else if(b < c) {
+				smallest = b;
+			}
+			else {
+				smallest = c;
+			}
+			
+			myFile.write(smallest + "  " + biggest +  "\n");
+		}
+		
+		myFile.close();
+		scan.close();
+		
+		System.out.println("Smallest and largest integers from " + inFile 
+			+ " written to " + outFile);
+	}
+	
+	public static void testLines(String inFile, String outFile) throws FileNotFoundException
+	{
+		PrintWriter myFile = new PrintWriter(outFile);
+		Scanner scan = new Scanner(new File(inFile));
+		String key = scan.nextLine();
+		
+		while(scan.hasNextLine()) {
+			String check = scan.nextLine();
+			if(check.toUpperCase().indexOf(key.toUpperCase()) != -1) {
+				myFile.write(check + "\n");
+			}
+		}
+		
+		myFile.close();
+		scan.close();
+		
+		System.out.println("Lines from " + inFile + " written to " + outFile);
+	}
+	
+	public static void extractSubstrings(String inFile, String outFile) throws FileNotFoundException
+	{
+		System.out.println("Requested substrings from " + inFile + " written to " + outFile);
+	}
+	
+	private static String getSubstring(int index1, int index2, String text)
+	{
+		return "";
+	}
+}
