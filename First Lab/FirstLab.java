5128ba8a2a35ca6c446f4d3efa3127b7027900c5
s-thnd <98992429+s-thnd@users.noreply.github.com>
2022-02-07 11:04:08 -0600

    Create FirstLab.java

---
+++ First Lab/FirstLab.java
@@ -0,0 +1,53 @@
+//CHANGE THIS LAB CODE BEFORE SUBMITTING!!!!!!!!
+//This is not a copy+pastable lab. Use this only as inspiration. 
+//You will have to center each name-- dont forget escape sequences!!
+//An easy way to do the bonus part is by copy pasting in ASCII art from the internet
+
+public class FirstLab 
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("*~~~~~~++~~~++~~~++~~~++~~~++~~~++~~~~~~*");
+		System.out.println("|            \"Shyam Thandullu\"          |");
+		System.out.println("*~~~~~~++~~~++~~~++~~~++~~~++~~~++~~~~~~*");
+		
+		System.out.println("");
+		
+		System.out.println("*~~~~~~++~~~++~~~++~~~++~~~++~~~++~~~~~~*");
+		System.out.println("|            \"Shyam Thandullu\"          |");
+		System.out.println("*~~~~~~++~~~++~~~++~~~++~~~++~~~++~~~~~~*");
+		System.out.println("|         Andy Zhu is really nice       |");
+		System.out.println("*~~~~~~++~~~++~~~++~~~++~~~++~~~++~~~~~~*");
+		System.out.println("|    Alec Fu is always fun to talk to   |");
+		System.out.println("*~~~~~~++~~~++~~~++~~~++~~~++~~~++~~~~~~*");
+		System.out.println("");		
+		
+		
+
+		
+		// This calls the bonus method to draw your silly face/picture
+		bonus();
+	}
+	
+	/** Draws a silly face or picture
+	 */	
+	public static void bonus()
+	{
+		System.out.println("+====================================+");
+		System.out.println("|                                    |");
+		System.out.println("|           \\ | /                    |");
+		System.out.println("|            \\|/		     |");
+		System.out.println("|           /~~~\\                    |");
+		System.out.println("|          /     \\                   |");
+		System.out.println("|  /\\     /~~~~~~~\\    /\\    /\\      |");
+		System.out.println("|  /\\    /         \\   /\\    /\\      |");
+		System.out.println("|  ||   /~~~~~~~~~~~\\  ||    ||	     |");
+		System.out.println("| ---------------------------------- |");
+		System.out.println("|    \"Volcano Erupting in Forest\"    |  ");
+		System.out.println("+====================================+");
+		
+		System.out.println("");
+		
+	}
+}
